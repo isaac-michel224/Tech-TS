e511c4cbbd5ed8483b926ea0c96d2c65af4f1f71
isaac-michel224 <84110054+isaac-michel224@users.noreply.github.com>
2021-07-04 17:44:25 -0400

    Add files via upload

---
+++ loop in javascript_Michel. 630.js
@@ -0,0 +1,26 @@
+//Loops
+
+var myArray = ["What is the meaning of life?", "The meaning of life is", 42];
+
+// TODO: modify this code
+for (var i = 0; i < myArray.length; i++)
+{
+    console.log(myArray[i]);
+}
+
+
+//For Loop - JS Practice
+
+for (var x=0; x<=99; x++) {
+    if (x === 0) {
+        console.log(x + " is even");
+    }
+    else if (x %2 === 0) {
+        console.log(x + " is even");
+    }
+    else {
+        console.log(x + " is odd");
+    }
+}
+
+
